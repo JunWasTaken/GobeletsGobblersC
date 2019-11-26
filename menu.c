4a7e18479d7223f2de8bac4947f0e00dc7a455e0
nootnoot <fheuef27@gmail.com>
2019-11-26 19:54:16 +0100

    ptites modifs inutiles lol

--- menu.c
+++ menu.c
@@ -4,20 +4,20 @@
 int menu(){
     int choix;
     int returnValue=0;
+
     printf("Bonjour et bienvenue dans Gobelet Gobblers, que voulez-vous faire ?\n1-Lancer une nouvelle partie\n2-gerer vos sauvegardes\n3-quitter\n");
+    
     do{
         printf("Votre Choix : ");
         scanf("%d", &choix);
-        if (choix==1){
-            returnValue= 1;
-        }else if (choix==2){
-            returnValue = 2;
-        }else if (choix==3)
-            returnValue = 0;
+        if (choix >= 1 && choix <= 3){
+            returnValue = choix%3;
+        }
         else
         {
             printf("Veuillez saisir une valeur valide\n");
         }
-    }while(choix!=1 && choix!=2 && choix!=3);
+    }while(choix < 1 && choix > 3);
+    
     return returnValue;
 }
