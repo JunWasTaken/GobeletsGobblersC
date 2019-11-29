65fb640a4ef568b6dedd49c0d9ecf445dcd8fcb6
Clément <azekool123@gmail.com>
2019-11-29 10:30:09 +0100

    J'ai un peu modifié le main

--- menu.c
+++ menu.c
@@ -38,17 +38,17 @@ void TourJeu(player x, board game){
     if (choix==1){
         printf("Quelle taille de pièce voulez-vous jouer ? Petite, Moyenne ou Grande\n(Faire un choix entre 1 et 3) -> ");
         pieceSize=choiceSelector(3);
-        /*while(!get_nb_piece_in_house(game, x, pieceSize)){
+        while(!get_nb_piece_in_house(game, x, pieceSize)){
             printf("Veuillez faire un choix valide\n");
             pieceSize=choiceSelector(3);
-        }*/
+        }
         printf("Dans quelle colonne voulez-vous la placer\n(Faire un choix entre 1 et 3) -> ");
         column=choiceSelector(3)-1;
         printf("Dans quelle ligne voulez-vous la placer\n(Faire un choix entre 1 et 3) -> ");
         row=choiceSelector(3)-1;
         if (pieceSize>get_piece_size(game, row, column)){
             printf("taille de la piece à la case souhaitée : %d\n", get_piece_size(game, row, column));
-            if (place_piece(game, x, SMALL, row, column))
+            if (place_piece(game, x, pieceSize, row, column))
                 printf("pièce placée avec succès\n");
             printf("test\n");
         }
