7c36b36c4346cf2df13e97a6c20b7d60047404da
Clément Denos <azekool123@gmail.com>
2019-11-29 12:18:57 +0100

    Modification de menu.c/.h avec test de commentaire et plus grande modularité du fichier

--- menu.c
+++ menu.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "board.h"
 
-int choiceSelector(int x){
+int choiceSelector(int x){ 
     int choix;
     int returnValue=0;
     
@@ -21,17 +21,45 @@ int choiceSelector(int x){
     return returnValue;
 }
 
-/*void affichagePlateau(board game){
+void affichagePlateau(board game){
     for (int i=0; i<DIMENSIONS; i++){
         for (int j=0; j<DIMENSIONS; j++){
             printf("%d ", get_piece_size(game, i, j));
         }
         printf("\n");
     }
-}*/
+}
 
-board placementPion(board game, player x){
-    printf("x");
+board placementPion(board game, player x){ //fonction gérant le placement d'une pièce sur le plateau
+    int column=0, row=0;
+    unsigned int pieceSize=0;
+    printf("Quelle taille de pièce voulez-vous jouer ? Petite, Moyenne ou Grande\n(Faire un choix entre 1 et 3) -> ");
+    pieceSize=choiceSelector(3);
+    while(!get_nb_piece_in_house(game, x, pieceSize)){
+        printf("Veuillez faire un choix valide\n");
+        pieceSize=choiceSelector(3);
+    }
+    printf("Dans quelle colonne voulez-vous la placer\n(Faire un choix entre 1 et 3) -> ");
+    column=choiceSelector(3)-1;
+
+    printf("Dans quelle ligne voulez-vous la placer\n(Faire un choix entre 1 et 3) -> ");
+    row=choiceSelector(3)-1;
+        
+    int res = place_piece(game, x, pieceSize, row, column);
+    switch (res){
+        case 0:
+            printf("Piece placée avec succès\n");
+            break;
+        case 1:
+            printf("Pas assez de pièces de cette taille dans la maison\n");
+            break;
+        case 2:
+            printf("La pièce est trop petite pour être placée\n");
+            break;
+        case 3:
+            printf("Position invalide\n");
+    }
+    affichagePlateau(game);
 }
 
 void TourJeu(player x, board game){
@@ -40,34 +68,12 @@ void TourJeu(player x, board game){
 
     printf("Que voulez-vous faire ?\n1-Placer un pion\n2-Déplacer un pion\n");
     choix = choiceSelector(2);
-
-    if (choix == 1){
-        printf("Quelle taille de pièce voulez-vous jouer ? Petite, Moyenne ou Grande\n(Faire un choix entre 1 et 3) -> ");
-        pieceSize=choiceSelector(3);
-        while(!get_nb_piece_in_house(game, x, pieceSize)){
-            printf("Veuillez faire un choix valide\n");
-            pieceSize=choiceSelector(3);
-        }
-        printf("Dans quelle colonne voulez-vous la placer\n(Faire un choix entre 1 et 3) -> ");
-        column=choiceSelector(3)-1;
-
-        printf("Dans quelle ligne voulez-vous la placer\n(Faire un choix entre 1 et 3) -> ");
-        row=choiceSelector(3)-1;
-        
-        int res = place_piece(game, x, pieceSize, row, column);
-        switch (res){
-            case 0:
-                printf("Piece placée avec succès\n");
-                break;
-            case 1:
-                printf("Pas assez de pièces de cette taille dans la maison\n");
-                break;
-            case 2:
-                printf("La pièce est trop petite pour être placée\n");
-                break;
-            case 3:
-                printf("Position invalide\n");
-        }
-        affichagePlateau(game);
+    switch (choix){
+        case 1:
+            game = placementPion(game, x);
+            break;
+        case 2:
+            affichagePlateau(game);
+            break;
     }
 }
--- menu.h
+++ menu.h
@@ -3,7 +3,17 @@
 
 #include "board.h"
 
+/**
+ * @brief fonction permettant de faire des choix allant de 1 à une borne x définie par l'utilisateur
+ * 
+ * Cette fonction est très utilisée dans le programme puisque l'utilisateur est amenée à faire de nombreux choix
+ * 
+ * @param x -> un entier définissant la borne
+ * @return la valeur choisie par l'utilisateur si elle rentre 
+ */
 int choiceSelector(int x);
+
+
 void TourJeu(player x, board game);
 
 #endif
