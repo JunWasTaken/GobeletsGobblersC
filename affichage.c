608a527be6116fc0b9a9205b4f8fb252fa54bbb6
nootnoot <fheuef27@gmail.com>
2019-11-29 12:20:01 +0100

    affichage.h

--- affichage.c
+++ affichage.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
 #include "board.h"
 
 #define BLEU "\033[0;94m"
@@ -9,19 +8,13 @@
 #define ROUGE "\033[0;31m"
 #define ROUGE_ "\033[1;31m"
 
-void color(int textColor, int bgColor){
-    HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(H,bgColor*16+textColor);
-}
 
 void affichagePlateau(board game){
     for (int i=0; i<DIMENSIONS; i++){
         for (int j=0; j<DIMENSIONS; j++){
             if (get_place_holder(game, i, j)==1){
-                color(3, 0);
                 printf("%d ", get_piece_size(game, i, j));
             }else{
-                color(12, 0);
                 printf(ROUGE"%d ", get_piece_size(game, i, j));
             }
             
---
+++ affichage.h
@@ -0,0 +1,8 @@
+#ifndef _AFFICHAGE_H_
+#define _AFFICAHGE_H_
+
+#include "board.h"
+
+void affichagePlateau(board game);
+
+#endif
