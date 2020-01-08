0552a445604536d97671822b6d66eaf7bbb47305
nootnoot <fheuef27@gmail.com>
2020-01-08 14:14:15 +0100

    haha

--- easyBot.c
+++ easyBot.c
@@ -6,22 +6,24 @@
 coord selectCase(){
     coord casePlateau;
     srand(time(NULL));
-    casePlateau.x = rand()%(3-1)+1;
-    casePlateau.y = rand()%(3-1)+1;
+
+    casePlateau.x = rand()%3;
+    casePlateau.y = rand()%3;
+
     return casePlateau;
 }
 
-void tourBot(board game){
-    coord casePlateau = selectCase();
-    int pieceSize = get_piece_size(game, casePlateau.x, casePlateau.y);
+void tourBot(board game, player pl){
+    coord casePlateau;
+    int pieceSize;
+    int res;
+
     do{
-        if (get_place_holder(game, casePlateau.x, casePlateau.y) != game->house[1].joueur){
-            if (pieceSize<3){
-                if (get_nb_piece_in_house(game, game->house[1].joueur, pieceSize+1)){
-                    place_piece(game, game->house[1].joueur, pieceSize, casePlateau.x, casePlateau.y);
-                }
-            }
-        }else 
-            casePlateau = selectCase();
-    }while(!place_piece(game, game->house[1].joueur, pieceSize, casePlateau.x, casePlateau.y));
+        casePlateau = selectCase();
+        pieceSize = get_piece_size(game, casePlateau.x, casePlateau.y);
+
+        if (get_place_holder(game, casePlateau.x, casePlateau.y) != pl){
+            res = place_piece(game, pl, pieceSize+1, casePlateau.x, casePlateau.y);
+        }
+    }while(res);
 }
